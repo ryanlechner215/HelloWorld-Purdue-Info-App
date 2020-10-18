2805846269987506f31114e278c39d479e7590c0
YoBerg <yohan520@gmail.com>
2020-10-17 19:18:36 -0700

    Combined title view with PurdueIO API code.

--- PurdueIOAndroidTest/app/src/main/java/com/example/purdueioandroidtest/MainActivity.java
+++ PurdueIOAndroidTest/app/src/main/java/com/example/purdueioandroidtest/MainActivity.java
@@ -2,23 +2,52 @@ package com.example.purdueioandroidtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import android.os.Bundle;
+import android.widget.ImageSwitcher;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void doLogin(View v) {
+        String username = ((EditText) findViewById(R.id.editTextUsername)).getText().toString();
+        String password = ((EditText) findViewById(R.id.editTextPassword)).getText().toString();
+
+
+
+
+        Toast.makeText(getApplicationContext(), username + " " + password, Toast.LENGTH_SHORT).show();
+
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        view.findViewById(R.id.getButton).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(FirstFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+//            }
+//        });
+
+        TextView textView = (TextView) findViewById(R.id.textView2);
         Button getButton = (Button) findViewById(R.id.getButton);
-        TextView resultsTextView = (TextView) findViewById(R.id.resultsTextView);
 
         getButton.setOnClickListener(view -> {
             try {
@@ -33,13 +62,13 @@ public class MainActivity extends AppCompatActivity {
                     StrictMode.setThreadPolicy(policy);
 
                     // Calling the API
-                    resultsTextView.setText(PurdueIOTest.callAPI(URL, ENDPOINT, PARAMS));
+                    textView.setText(PurdueIOTest.callAPI(URL, ENDPOINT, PARAMS));
                 }
 
             } catch (IOException e) {
                 final String ERROR = "Invalid API address.";
                 e.printStackTrace();
-                resultsTextView.setText(ERROR);
+                textView.setText(ERROR);
             }
         });
     }
